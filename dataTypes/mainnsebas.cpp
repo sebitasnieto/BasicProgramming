e8e8f1ef90c09394ab9f4346961193e8415ff6b1
sebitasnieto <160760075+sebitasnieto@users.noreply.github.com>
2024-02-21 11:06:08 -0500

    numbers

---
+++ dataTypes/mainnsebas.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	float f1 = 35E3;
+	double d1 = 12E4;
+	cout << f1 <<endl;
+	cout << d1 <<endl;
+	return 0;
+}
