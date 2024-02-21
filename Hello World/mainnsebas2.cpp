619f1b92850e3e478eb9020e5c716baf6937f140
sebitasnieto <160760075+sebitasnieto@users.noreply.github.com>
2024-02-21 10:59:08 -0500

    mi primer proyecto: Hello world

---
+++ Hello World/mainnsebas2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	printf("Hello world\n");
+	return 0;
+}
