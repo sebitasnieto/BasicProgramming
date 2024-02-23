7933dfcf6ed1197cb3b55c273d6fd72087fdc98e
sebitasnieto <160760075+sebitasnieto@users.noreply.github.com>
2024-02-23 12:34:52 -0500

    Add files via upload

---
+++ dataTypes/tarea10.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+    string greeting = "Hello";
+    cout << greeting;
+	return 0;
+}
