5cac4e80ccdffe2f55fea6e9c5ae62f5417e0a5d
sebitasnieto <160760075+sebitasnieto@users.noreply.github.com>
2024-02-23 11:49:34 -0500

    Add files via upload

---
+++ dataTypes/Example 1 basic data types.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	//Creating variables (creacion de variables)
+	
+  int myNum = 5;               // Integer (whole number)
+  float myFloatNum = 5.99;     // Floating point number
+  double myDoubleNum = 9.98;   // Floating point number
+  char myLetter = 'D';         // Character
+  bool myBoolean = true;       // Boolean
+  string myString = "Hello";   // String
+   
+  // Print variable values (imprimir el valor de las variables)
+  cout << "int: " << myNum << "\n";
+  cout << "float: " << myFloatNum << "\n";
+  cout << "double: " << myDoubleNum << "\n";
+  cout << "char: " << myLetter << "\n";
+  cout << "bool: " << myBoolean << "\n";
+  cout << "string: " << myString << "\n";
+	return 0;
+}
