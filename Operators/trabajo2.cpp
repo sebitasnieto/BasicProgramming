d0d192ffb19a6c82a75dcaab067596fc6e5bc271
sebitasnieto <160760075+sebitasnieto@users.noreply.github.com>
2024-02-23 14:14:09 -0500

    Add files via upload

---
+++ Operators/trabajo2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int sum1 = 100 + 50;        // 150 (100 + 50)
+    int sum2 = sum1 + 250;      // 400 (150 + 250)
+    int sum3 = sum2 + sum2;     // 800 (400 + 400)
+    cout<<sum3;
+	return 0;
+}
