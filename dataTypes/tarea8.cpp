2d41f7b1ee6d0c05678ec79c327119aee16513b3
sebitasnieto <160760075+sebitasnieto@users.noreply.github.com>
2024-02-23 12:19:30 -0500

    Add files via upload

---
+++ dataTypes/tarea8.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	// ASCII values
+	char a = 65, b = 66, c = 67;
+    cout << a;
+    cout << b;
+    cout << c;
+	return 0;
+}
