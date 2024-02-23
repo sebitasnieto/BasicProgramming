5400945fd191ea809d8618df416b83581abb4e6b
sebitasnieto <160760075+sebitasnieto@users.noreply.github.com>
2024-02-23 11:48:46 -0500

    Add files via upload

---
+++ dataTypes/Exercise1basicdatatypes.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	    //Exercise 1 Basic Data types
+	int myNum = 9;
+	double myDoublenum = 8.99;
+	string myText = "Hello world";
+	char myLetter = 'A';
+	bool myBool = false;
+	return 0;
+}
