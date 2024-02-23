56b666f6e84b8df8d800c2dae3da4a1daf32d2a7
sebitasnieto <160760075+sebitasnieto@users.noreply.github.com>
2024-02-23 12:40:21 -0500

    Add files via upload

---
+++ dataTypes/tarea11.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	//exercise boolean variables
+	bool nay = true; 
+	bool yay = false;
+	return 0;
+}
