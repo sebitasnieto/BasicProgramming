4992b6d91bf0ccafcf48be48bd88c8851fa94ccd
sebitasnieto <160760075+sebitasnieto@users.noreply.github.com>
2024-02-23 12:13:52 -0500

    Add files via upload

---
+++ dataTypes/tarea6.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+  bool isCodingFun = true;
+  bool isFishTasty = false;
+  cout << isCodingFun << "\n";
+  cout << isFishTasty;
+	return 0;
+}
