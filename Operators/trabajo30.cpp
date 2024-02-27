ffcea1b7385f78ffd21904d11598f9077ac13cc8
sebitasnieto <160760075+sebitasnieto@users.noreply.github.com>
2024-02-26 19:26:03 -0500

    Add files via upload

---
+++ Operators/trabajo30.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 5;
+  int y = 3;
+  cout << (x >= y); // returns 1 (true) because five is greater than, or equal, to 3
+  return 0;
+}
