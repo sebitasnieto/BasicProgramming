6c82306834ff626ee650e453a5207da2fc6213f1
sebitasnieto <160760075+sebitasnieto@users.noreply.github.com>
2024-03-06 10:58:06 -0500

    Add files via upload

---
+++ Loops/for/proye1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	for (int i = 0; i < 5; i++) {
+    cout << i << "\n";
+}
+	return 0;
+}
