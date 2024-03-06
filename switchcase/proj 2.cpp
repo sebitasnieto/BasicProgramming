88fc1439b75a017a11492c2d0cf15020f8f2e2c3
sebitasnieto <160760075+sebitasnieto@users.noreply.github.com>
2024-03-06 10:40:17 -0500

    Add files via upload

---
+++ switchcase/proj 2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int day = 4;
+	switch (day){
+		case 6:
+	cout <<"Today is Saturday";
+	break;
+		case 7:
+	cout <<"Today is Sunday";
+	break;
+	default:
+	cout <<"Looking forward to the weekend";
+	return 0;
+	}
+}
