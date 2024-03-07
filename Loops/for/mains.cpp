45f0d13d7f03083e80f0459fdbcdb174dc500eb3
sebitasnieto <160760075+sebitasnieto@users.noreply.github.com>
2024-03-07 06:52:31 -0500

    Add files via upload

---
+++ Loops/for/mains.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int i=0;
+	if (i<10){
+	cout<<i<<endl;
+	}
+	if (i<10){
+		i=i+1;
+	cout<<i<<endl;
+	}
+	if (i<10){
+		i=i+1;
+	cout<<i<<endl;
+	}
+	if (i<10){
+		i=i+1;
+	cout<<i<<endl;
+	}
+	if (i<10){
+		i=i+1;
+	cout<<i<<endl;
+	}
+	if (i<10){
+		i=i+1;
+	cout<<i<<endl;
+	}
+	if (i<10){
+		i=i+1;
+	cout<<i<<endl;
+	}
+	if (i<10){
+		i=i+1;
+	cout<<i<<endl;
+	}
+	if (i<10){
+		i=i+1;
+	cout<<i<<endl;
+	}
+	if (i<10){
+		i=i+1;
+	cout<<i<<endl;
+	}
+}
