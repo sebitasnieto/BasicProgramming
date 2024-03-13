9c12af45bb91f3e256a9b71f7b62aeab5ba62283
sebitasnieto <160760075+sebitasnieto@users.noreply.github.com>
2024-03-13 10:54:28 -0500

    Add files via upload

---
+++ whileloop/traba3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+   int i=1;
+   /* el valor de i se muestra mientras se cumpla la condicion
+    * i<=6 se vuelve falso
+    */
+   while(i<=6){
+      cout<<"El valor de la variable i es: "<<i<<endl; i++;
+   }
+}
