77fc4858188e59aa0eede59dbf36a7f3dc0bde6c
sebitasnieto <160760075+sebitasnieto@users.noreply.github.com>
2024-03-13 11:00:28 -0500

    Add files via upload

---
+++ whileloop/traba5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+   int arr[]={21,87,15,99, -12};
+   /*Se usa contador para mostrar todos los valores de la matriz, usando ademas el ciclo while*/
+   int i=0;
+   while(i<5){
+      cout<<arr[i]<<endl;
+      i++;
+   }
+}
