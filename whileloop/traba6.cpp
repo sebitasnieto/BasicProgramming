86f435d515133b7e80f82912ad112f9e64ec55a8
sebitasnieto <160760075+sebitasnieto@users.noreply.github.com>
2024-03-13 11:09:10 -0500

    Add files via upload

---
+++ whileloop/traba6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	//declaracion local de variable:
+	int a=10;
+	
+	//Ejecucion ciclo while del numero 10 a 19;
+	while (a<20) {
+		cout << "el valor de a es: " << a << endl;
+		a++;
+	}
+	return 0;
+}
