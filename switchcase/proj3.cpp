9252c49e9fe5b41f0497bc350de9f84c06752d4f
sebitasnieto <160760075+sebitasnieto@users.noreply.github.com>
2024-03-12 22:12:39 -0500

    Add files via upload

---
+++ switchcase/proj3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int day = 1;
+	switch (day) {
+  case 1:
+    cout << "Saturday";
+    break;
+  case 2:
+    cout << "Sunday"; 
+	break;
+    }
+	return 0;
+}
