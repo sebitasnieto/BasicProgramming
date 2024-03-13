c9e34e1818551f906e4a914e897a2b287865c80c
sebitasnieto <160760075+sebitasnieto@users.noreply.github.com>
2024-03-13 10:57:36 -0500

    Add files via upload

---
+++ whileloop/traba4.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+   int i=1; while(i<=6) {
+   	/*ejemplo de un bucle infinito*/;
+   	cout<<"El valor de la variable i es: "<<i<<endl; i--;
+   }
+}
