7429b6497c7a2cff4d0ccfd2f2aaf5b7125ba30e
sebitasnieto <160760075+sebitasnieto@users.noreply.github.com>
2024-03-16 05:13:49 -0500

    Add files via upload

---
+++ variables/deber3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	char myLetter= 'j';// Character string myText="Hello"; 
+	bool myBoolean=true; //Boolean (true or false)
+	
+	return 0;
+}
