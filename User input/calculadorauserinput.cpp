dd369fb9ded7c85959da6f729e35fd1fdb0d9e21
sebitasnieto <160760075+sebitasnieto@users.noreply.github.com>
2024-03-16 05:05:01 -0500

    Add files via upload

---
+++ User input/calculadorauserinput.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std; 
+
+int main() {
+    int	x;
+	cout << "escribe un numero:";
+	cin >> x;
+	cout <<"tu numero es:" << x<<endl;
+		
+	
+	int a,b;
+	int sum;
+	cout <<"escribe un numero:";
+	cin >> a;
+	cout << "escribe otro numero:";
+	cin >> b;
+	sum= a+b;
+	cout << "la suma da un total de:"<<sum;
+	return 0;
+}
