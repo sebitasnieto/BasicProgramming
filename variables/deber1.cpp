db47d4c47dbd11f70af093d655a7e7179a4bc44a
sebitasnieto <160760075+sebitasnieto@users.noreply.github.com>
2024-03-16 05:09:53 -0500

    Add files via upload

---
+++ variables/deber1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+/* run this program using the consolo pauser or add your own getch, system("pause") or input loop */
+
+int  main(int argc, char**argv) {
+	 
+    int myNum=15; // myNum is 15 
+	myNum=10;// Now myNum is 10 
+	cout << myNum<<endl; // outputs 10
+	
+	return 0;
+}
